701df93399eb87e196585f010b4dfaac587b288c
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-08-10 18:40:24 +0200

    Improve HelpExec output formatting

--- src/main/java/com/cavetale/manager/command/HelpExec.java
+++ src/main/java/com/cavetale/manager/command/HelpExec.java
@@ -22,24 +22,25 @@ public final class HelpExec extends Exec {
     public void run() {
         Console.out(Style.HELP, EscCode.BOLD +
                 "-------------------------------------- Help --------------------------------------\n" +
-                EscCode.WEIGHT_OFF + """
-                Interactive: java -jar Manager.jar
-                Single: java -jar Manager.jar <1+command(s)> <0+flag(s)>
-                ------------------------------------ Commands ------------------------------------
-                """);
-        Console.outF(Style.HELP, EscCode.BOLD, "%2s %-13s | %-30s | %-30s%n",
+                EscCode.WEIGHT_OFF + "Interactive: java -jar Manager.jar\n" +
+                "Single: java -jar Manager.jar <1+command(s)> <0+flag(s)>\n" + EscCode.BOLD +
+                "\n------------------------------------ Commands ------------------------------------\n");
+        Console.outF(Style.HELP, "%2s %-13s | %-30s | %-30s%n",
                 "", "Command", "Info", "");
-        Console.out(Style.HELP, "----------------------------------------------------------------------------------\n");
+        Console.out(Style.HELP,
+                "----------------------------------------------------------------------------------\n");
         ArrayList<Command> commands = new ArrayList<>(List.of(Command.values()));
         Collections.sort(commands);
         for (Command c : commands) {
             Console.outF(Style.HELP, "%2s %-13s | %-30s | %-30s%n",
                     "", c.ref, c.description, "");
         }
-        Console.out(Style.HELP, "------------------------------------- Flags --------------------------------------\n");
-        Console.outF(Style.HELP, EscCode.BOLD + "%2s %-13s | %-30s | %-30s%n",
+        Console.out(Style.HELP,  EscCode.BOLD +
+                "\n------------------------------------- Flags --------------------------------------\n");
+        Console.outF(Style.HELP, "%2s %-13s | %-30s | %-30s%n",
                 "", "Flag", "Info", "Usage");
-        Console.out(Style.HELP, "----------------------------------------------------------------------------------\n");
+        Console.out(Style.HELP,
+                "----------------------------------------------------------------------------------\n");
         ArrayList<Flag> flags = new ArrayList<>(List.of(Flag.values()));
         Collections.sort(flags);
         for (Flag f : flags) {
