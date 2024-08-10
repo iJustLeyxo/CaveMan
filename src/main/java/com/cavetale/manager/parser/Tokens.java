abd1702c1a47ca282410c9ea95a98f01de090fdf
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-08-11 00:13:56 +0200

    Update token analysis

--- src/main/java/com/cavetale/manager/parser/Tokens.java
+++ src/main/java/com/cavetale/manager/parser/Tokens.java
@@ -5,6 +5,7 @@ import com.cavetale.manager.parser.container.EmptyContainer;
 import com.cavetale.manager.util.console.Console;
 import com.cavetale.manager.util.console.Detail;
 import com.cavetale.manager.util.console.Style;
+import com.cavetale.manager.util.console.XCode;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
@@ -15,30 +16,27 @@ public record Tokens (
         @NotNull Map<Flag, EmptyContainer> flags
 ) {
     public boolean analyse() {
-        Console.log(Style.DEBUG, "Analysing tokens\n");
-        boolean changed = false;
+        Console.log(Style.DEBUG, "Analysing tokens");
+        StringBuilder s = new StringBuilder();
         if (this.flags().containsKey(Flag.VERBOSE)) {
             Console.detail = Detail.HIGH;
-            Console.log(Style.INFO, "Verbose mode activated\n");
-            changed = true;
+            s.append("Verbose mode activated\n");
         } else if (this.flags().containsKey(Flag.DEFAULT)) {
             Console.detail = Detail.DEFAULT;
-            Console.log(Style.INFO, "Default verbosity mode activated\n");
-            changed = true;
+            s.append("Default verbosity mode activated\n");
         } else if (this.flags().containsKey(Flag.QUIET)) {
             Console.detail = Detail.LOW;
-            Console.log(Style.INFO, "Quiet mode activated\n");
-            changed = true;
+            s.append("Quiet mode activated\n");
         }
         if (this.flags().containsKey(Flag.INTERACTIVE) && !Manager.interactive) {
             Manager.interactive = true;
-            Console.log(Style.INFO, "Interactive mode activated\n");
-            changed = true;
+            s.append("Interactive mode activated\n");
         }
-        if (changed) {
-            Console.log(Style.INFO, "\n");
+        Console.log(Style.DEBUG, XCode.GREEN + " done\n\n");
+        if (!s.isEmpty()) {
+            Console.log(Style.INFO, s + "\n");
+            return true;
         }
-        Console.log(Style.DEBUG, "Finished analysing tokens\n\n");
-        return changed;
+        return false;
     }
 }
