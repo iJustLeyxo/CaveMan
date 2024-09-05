97e64c341ba2885c7f91ba5a74b3ebec81a143ac
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-09-05 09:19:03 +0200

    Update comment

--- src/main/java/com/cavetale/manager/data/DataException.java
+++ src/main/java/com/cavetale/manager/data/DataException.java
@@ -3,7 +3,7 @@ package com.cavetale.manager.data;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Data error, used for severe issues with hardcoded data
+ * Data exception, used for minor issues with data handling
  */
 public abstract class DataException extends Exception {
     public DataException(@NotNull String message) {
